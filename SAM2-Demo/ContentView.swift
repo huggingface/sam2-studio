f92c954df9dc3a235fb03ae010d7bb66a4fa67a4
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-08-19 09:46:50 -0700

    Initial Commit

---
+++ SAM2-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SAM2-Demo
+//
+//  Created by Cyril Zakka on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SAM2-Demo/SAM2_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAM2_DemoApp.swift
+//  SAM2-Demo
+//
+//  Created by Cyril Zakka on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SAM2_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
