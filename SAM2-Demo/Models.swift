50c059c433cc2a4d5ddaf7c1b601b84532a2637b
Pedro Cuenca <pedro@huggingface.co>
2024-08-28 13:37:59 +0200

    Support boxes
    By converting to points.

--- SAM2-Demo/Models.swift
+++ SAM2-Demo/Models.swift
@@ -9,15 +9,21 @@ import Foundation
 import SwiftUI
 
 enum SAMCategoryType: Int {
-    case foreground = 1
     case background = 0
-    
+    case foreground = 1
+    case boxOrigin = 2
+    case boxEnd = 3
+
     var description: String {
         switch self {
         case .foreground:
             return "Foreground"
         case .background:
             return "Background"
+        case .boxOrigin:
+            return "Box Origin"
+        case .boxEnd:
+            return "Box End"
         }
     }
 }
@@ -32,6 +38,34 @@ struct SAMCategory: Hashable {
     var typeDescription: String {
         type.description
     }
+
+    static let foreground = SAMCategory(
+        type: .foreground,
+        name: "Foreground",
+        iconName: "square.on.square.dashed",
+        color: .pink
+    )
+
+    static let background = SAMCategory(
+        type: .background,
+        name: "Background",
+        iconName: "square.on.square.intersection.dashed",
+        color: .purple
+    )
+
+    static let boxOrigin = SAMCategory(
+        type: .boxOrigin,
+        name: "Box Origin",
+        iconName: "",
+        color: .white
+    )
+
+    static let boxEnd = SAMCategory(
+        type: .boxEnd,
+        name: "Box End",
+        iconName: "",
+        color: .white
+    )
 }
 
 
@@ -48,6 +82,12 @@ struct SAMBox: Hashable, Identifiable {
     let category: SAMCategory
 }
 
+extension SAMBox {
+    var points: [SAMPoint] {
+        [SAMPoint(coordinates: startPoint, category: .boxOrigin), SAMPoint(coordinates: endPoint, category: .boxEnd)]
+    }
+}
+
 struct SAMTool: Hashable {
     let id: UUID = UUID()
     let name: String
@@ -59,17 +99,3 @@ let normalTool: SAMTool = SAMTool(name: "Selector", iconName: "cursorarrow")
 let pointTool: SAMTool = SAMTool(name: "Point", iconName: "hand.point.up.left")
 let boundingBoxTool: SAMTool = SAMTool(name: "Bounding Box", iconName: "rectangle.dashed")
 let eraserTool: SAMTool = SAMTool(name: "Eraser", iconName: "eraser")
-
-let foregroundCat: SAMCategory = SAMCategory(
-    type: .foreground,
-    name: "Foreground",
-    iconName: "square.on.square.dashed",
-    color: .pink
-)
-
-let backgroundCat: SAMCategory = SAMCategory(
-    type: .background,
-    name: "Background",
-    iconName: "square.on.square.intersection.dashed",
-    color: .purple
-)
